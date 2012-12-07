2e9db008ab025da02957ad3281f1f214e9890834
Kaspar Bumke <kaspar.bumke@gmail.com>
2012-12-07 17:22:52 +0000

    adding license info and readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# SphereWare #
+
+SphereWare is the AlphaSphere and AlphaSphere Elite Firmware. 
+
+## Hardware ##
+
+AlphaSphere uses an Atmel Atmega32u4 MCU clocked at 16Mhz.
+
+## Requirements ##
+To build this software you will need the avr-gcc toolchain.
+
+## License ##
+
+SphereWare is licensed under the GPLv3 or later. See GPL3.txt for details.
+
+SphereWare uses the [LUFA](http://www.fourwalledcubicle.com/LUFA.php) USB stack which is licensed under the MIT license. 
--- SphereWare/DigPot.c
+++ SphereWare/DigPot.c
@@ -1,3 +1,21 @@
+/* Copyright (c) nu desine 2012 
+
+    This file is part of SphereWare.
+
+    SphereWare is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SphereWare is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 #include "DigPot.h" 
 
 void DigPot_Init(void)
