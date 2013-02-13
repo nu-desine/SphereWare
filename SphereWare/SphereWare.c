b7cfe49881040531fafa6e4567c8bbaea5b191aa
Kaspar Bumke <kaspar.bumke@gmail.com>
2013-02-13 19:47:20 +0000

    buttons and dials working at HID 0.1.0

--- SphereWare/SphereWare.c
+++ SphereWare/SphereWare.c
@@ -72,15 +72,82 @@ uint8_t pads_under_test[NUM_OF_PUTS];// = {0, 1};//, 2, 3, 4, 5};
 //uint8_t dud_pads[NUM_OF_DUDS] = {32, 33, 34, 39, 40, 41};
 
 /* returns change in encoder state (-1,0,1) */
-//int8_t read_encoder()
-//{
-//  static int8_t enc_states[] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0}; //static!
-//  static uint8_t old_AB = 0;
-//  /**/
-//  old_AB <<= 2;                   //remember previous state
-//  old_AB |= ( ENC_PORT & 0x03 );  //add current state
-//  return ( enc_states[( old_AB & 0x0f )]);
-//}
+uint8_t read_encoder(uint8_t decoder_val)
+{
+  //static uint8_t enc_states[] = { 
+  //                                000,
+  //                                000, 
+  //                                000, 
+  //                                000, 
+  //                                000,
+  //                                000,
+  //                                000,
+  //                                127,
+  //                                000,
+  //                                000,
+  //                                000,
+  //                                001,
+  //                                000,
+  //                                000,
+  //                                000,
+  //                                000
+  //};
+      //0000 000
+      //0001 000
+      //0010 000
+      //0011 000
+      //0100 000
+      //0101 000
+      //0110 000
+      //000111 127
+      //1000 000
+      //1001 000
+      //1010 000
+      //001011 001
+      //1100 000
+      //1101 000
+      //1110 000
+      //1111 000
+      
+  static uint8_t old_AB = 0;
+  /**/
+  old_AB <<= 2;                   //remember previous state
+  old_AB |= decoder_val & 0b11; //add current state
+  if ((old_AB & 0b111111) == 0b001011)
+      return 1;
+  else if ((old_AB & 0b111111) == 0b000111)
+      return 127;
+  else
+      return 0;
+}
+
+uint8_t read_encoder_1(uint8_t decoder_val)
+{
+  static uint8_t old_AB = 0;
+  /**/
+  old_AB <<= 2;                   //remember previous state
+  old_AB |= decoder_val & 0b11; //add current state
+  if ((old_AB & 0b111111) == 0b001011)
+      return 127;
+  else if ((old_AB & 0b111111) == 0b000111)
+      return 1;
+  else
+      return 0;
+}
+
+uint8_t read_encoder_2(uint8_t decoder_val)
+{
+  static uint8_t old_AB = 0;
+  /**/
+  old_AB <<= 2;                   //remember previous state
+  old_AB |= decoder_val & 0b11; //add current state
+  if ((old_AB & 0b111111) == 0b001011)
+      return 127;
+  else if ((old_AB & 0b111111) == 0b000111)
+      return 1;
+  else
+      return 0;
+}
 
 
 /** Main program entry point. This routine configures the hardware required by the application, then
@@ -91,7 +158,24 @@ int main(void)
     bool sensors_ok = true;
     int led_channels[3];
     uint8_t button_dial_report = 0;
+    uint8_t prev_button_dial_report = 0;
     uint8_t pins = 0;
+
+    bool button_1 = false;
+    bool button_2 = false;
+    bool button_3 = false;
+    uint8_t enc1_encoded = 0;
+    uint8_t enc2_encoded = 0;
+    uint8_t enc1 = 0;
+    uint8_t enc2 = 0;
+
+    bool prev_button_1 = false;
+    bool prev_button_2 = false;
+    bool prev_button_3 = false;
+    uint8_t prev_enc1_encoded = 0;
+    uint8_t prev_enc2_encoded = 0;
+    uint8_t prev_enc1 = 0;
+    uint8_t prev_enc2 = 0;
 
     for (int i = 0; i < NUM_OF_PUTS; ++i)
         pads_under_test[i] = i;
@@ -115,25 +199,60 @@ int main(void)
             switch (elite_mux_num)
             {
                 case SW2:
+                    button_1 = !(((PIND >> PD6)) & 1);
+                    if (button_1 != prev_button_1)
+                    {
+                        HidInReports_Create_Button_Report(0, button_1);
+                        prev_button_1 = button_1;
+                    }
+                    break;
                 case SW3:
+                    button_2 = !(((PIND >> PD6)) & 1);
+                    if (button_2 != prev_button_2)
+                    {
+                        HidInReports_Create_Button_Report(1, button_2);
+                        prev_button_2 = button_2;
+                    }
+                    break;
                 case SW4:
-                    button_dial_report &= ~(1 << elite_mux_num);
-                    button_dial_report |= (((PIND >>  PD6) & 1) << elite_mux_num);
+                    button_3 = !(((PIND >> PD6)) & 1);
+                    if (button_3 != prev_button_3)
+                    {
+                        HidInReports_Create_Button_Report(2, button_3);
+                        prev_button_3 = button_3;
+                    }
                     break;
                 case ENC1B:
-                    button_dial_report &= ~(1 << 4) & ~(1 << 5);
                     pins = PIND;
-                    button_dial_report |= (((pins >> PD6) & 1) << 4);
-                    button_dial_report |= (((pins >> PD0) & 1) << 5);
+                    enc1_encoded =  ((pins >> PD6) & 1) << 1 | (pins & 1);
+                    if (enc1_encoded != prev_enc1_encoded)
+                    {
+                        enc1 = read_encoder_1(enc1_encoded);
+                        if ((enc1 > 0) && (enc1 != prev_enc1))
+                        {
+                            //HidInReports_Create_Dial_Report(0, enc1_encoded);
+                            HidInReports_Create_Dial_Report(0, enc1);
+                        }
+                        prev_enc1 = enc1;
+                        prev_enc1_encoded = enc1_encoded;
+                    }
                     break;
                 case ENC2B:
-                    button_dial_report &= ~(1 << 6) & ~(1 << 7);
                     pins = PIND;
-                    button_dial_report |= (((pins >> PD6) & 1) << 6);
-                    button_dial_report |= (((pins >> PD1) & 1) << 7);
+                    enc2_encoded =  ((pins >> PD6) & 1) << 1 | ((pins >> PD1) & 1);
+                    if (enc2_encoded != prev_enc2_encoded)
+                    {
+                        enc2 = read_encoder_2(enc2_encoded);
+                        if ((enc2 > 0) && (enc2 != prev_enc2))
+                        {
+                            HidInReports_Create_Dial_Report(1, enc2);
+                        }
+                        prev_enc2 = enc2;
+                        prev_enc2_encoded = enc2_encoded;
+                    }
                     break;
             }
-            HidInReports_Create_Pad_Report(0, 0, button_dial_report);
+
         }
 
         //MUX_Select(ENC2B);
--- Utilities/hid-monitor/hid-monitor.py
+++ Utilities/hid-monitor/hid-monitor.py
@@ -5,6 +5,7 @@ import subprocess
 import os
 import datetime
 import time
+from pylab import *
 
 try:
     look_at_pad = sys.argv[1].split(":")
@@ -24,7 +25,8 @@ except IndexError:
 print look_at_pad
 print "Opening device"
 h = hid.device(0x1d50, 0x6021)
-h.set_nonblocking(1)
+h.set_nonblocking(0)
+
 
 #now = datetime.datetime.now()
 
@@ -43,21 +45,72 @@ h.set_nonblocking(1)
 #    print ""
 #
 
+#fixed decimal to fixed digit binary
+def bin(x, digits=0):
+    oct2bin = ['000','001','010','011','100','101','110','111']
+    binstring = [oct2bin[int(n)] for n in oct(x)]
+    return ''.join(binstring).lstrip('0').zfill(digits) 
+
 
 data = [] 
+velocities = [0] * 48
 try:
     #counter = 0;
     while 1:
         report = h.read(5)
-        try:
-            if report[0] == 0x01:
-                    decoded = (report[1], struct.unpack("h", "".join(map(chr, report[2:4])))[0], report[4])
-                    if report[1] in look_at_pad:
-                        data.append(decoded[1])
-                        print decoded
-        except IndexError:
-            time.sleep(0.001)
-            pass
+        print report
+        print bin(report[4], 8)
+        #if report[0] == 0x01:
+        #    for i in range(report[1]):
+        #        decoded = (report[(i*4)+2], struct.unpack("h", "".join(map(chr, report[(i*4) + 3:(i*4) + 5])))[0], report[(i*4) + 5]) 
+        #        if decoded[0] in look_at_pad:
+        #            print decoded
+        ###try:
+        #if report[0] == 0x01:
+        #    decoded = (report[1], struct.unpack("h", "".join(map(chr, report[2:4])))[0], report[4]) 
+        #    if report[1] in look_at_pad:
+        #        print decoded
+
+        #for i in range(1, 145, 3):
+        #    pad = i / 3
+        #    value = struct.unpack("h", "".join(map(chr, report[i:i+2])))[0]
+        #    velocity = report[i+2]
+        #    velocities[pad] = (velocity, pad)
+
+        #    print pad, value, velocity
+
+            #if (velocity != 0):
+            #    velocities[pad] = report[i+2]
+            #print pad, value, velocities[pad]
+                #decoded = (report[1], struct.unpack("h", "".join(map(chr, report[i + 2: i + 4])))[0], report[i + 3]) 
+
+
+            #for char in report[1:256]:
+            #    if char !=0:
+            #        print char
+
+            #if report[1] in look_at_pad:
+            #    #if decoded[2] != 0:
+            #    data.append(decoded[1])
+            #    print decoded
+                #print bin(decoded[2], 8), decoded
+        #if report[0] == 0x04:
+        #    for i in range(2, len(report)-1, 2):
+        #        #print report[i:i+2]
+        #        decoded = struct.unpack("h", "".join(map(chr, report[i:i+2])))[0]
+        #        data.append(-decoded)
+        #    print average(data)
+        #    #plot(data)
+        #    #index = data.index(min(data))
+        #    #velo = min(data)
+        #    #if index > 80:
+        #    #    print -velo
+        #    data = []
+
+
+        #except IndexError:
+        #    time.sleep(0.001)
+        #    pass
         #f.write(str(decoded) + '\n')
         #buf = [ 0x00, 0x06, 0x90, (counter & 0xFF), 0x00] 
         #h.write(buf)
