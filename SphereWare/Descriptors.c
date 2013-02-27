04d69cc69ad28bf1b71bbc86ff54fe3527601597
Kaspar Bumke <kaspar.bumke@gmail.com>
2013-02-27 14:13:37 +0000

    correcting bootloader descriptor

--- SphereWare/Descriptors.c
+++ SphereWare/Descriptors.c
@@ -99,7 +99,7 @@ const USB_Descriptor_Device_t PROGMEM DeviceDescriptor =
 
     .ManufacturerStrIndex   = 0x01,
     .ProductStrIndex        = 0x02,
-    .SerialNumStrIndex      = NO_DESCRIPTOR,
+    .SerialNumStrIndex      = USE_INTERNAL_SERIAL,
 
     .NumberOfConfigurations = FIXED_NUM_CONFIGURATIONS
 };
